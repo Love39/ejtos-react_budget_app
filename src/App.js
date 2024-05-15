57ed9970fb04ba4389ba584772d2858a5e7c26d8
Love39 <oziomalove1212@gmail.com>
2024-05-14 20:09:13 -0400

    changes made to budget app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 
 import { AppProvider } from './context/AppContext';
@@ -17,18 +21,34 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                             //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                             //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                             //ExpenseList component
+                             <div className='col-sm'>
+                                 <ExpenseList />
+                             </div>
                         }         
 
                         {
@@ -37,6 +57,10 @@ const App = () => {
 
                         {
                             /* Add AllocationForm component here under */
+                             //AllocationForm component
+                             <div className='col-sm'>
+                                 <AllocationForm />
+                             </div>
                         }        
 
                 </div>
